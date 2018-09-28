2f3d4c42143f0116873728afc2cb4f5e79fa63b5
aleshunina <morphia00@gmail.com>
2018-09-28 15:47:31 +0300

    commit refactor

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import selector.sorting.getmaxvalue;
+import org.testng.Assert;
+import utility.ElementMaxValue;
 
 public class SearchPage {
 
+    private static final String URL="https://www.autohero.com/de/search/";
     private WebDriver driver;
-    public static final String URL="https://www.autohero.com/de/search/";
-
     @FindBy(xpath = "//div[@data-qa-selector='filter-year']")
     private WebElement yearFilter;
 
@@ -42,13 +42,13 @@ public class SearchPage {
 
     public SearchPage filterByYear(String year){
 
-yearFilter.click();
-yearFilterSelector.click();
+        yearFilter.click();
+        yearFilterSelector.click();
         WebElement choosevaluefilter = driver.findElement(By.xpath("//option[@data-qa-selector-value='"+year+"']"));
         choosevaluefilter.click();
         yearFilterSelector.click();
 
-        WebElement filter = (new WebDriverWait(driver, 10))
+        WebElement waiter = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@data-qa-selector-value='"+year+"'and@data-qa-selector='active-filter']")));
 
         return new SearchPage(driver);
@@ -56,15 +56,32 @@ yearFilterSelector.click();
     }
 
     public SearchPage filterByBiggestPrice () {
-        getmaxvalue value=new getmaxvalue();
+
+        ElementMaxValue value = new ElementMaxValue();
 
         priceFilter.click();
         priceFilterSelector.click();
         WebElement choosevaluefilter = driver.findElement(By.xpath("//option[@data-qa-selector-value='"+value.sortmaxvalue(driver, By.xpath("//select[@data-qa-selector='select'and@name='priceRange.max']//option"))+"']"));
         choosevaluefilter.click();
         priceFilterSelector.click();
-        WebElement filter = (new WebDriverWait(driver, 10))
+        WebElement waiter = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@data-qa-selector-value='100.000'and@data-qa-selector='active-filter']")));
+
+        return new SearchPage(driver);
+    }
+
+    public SearchPage assertYearFilterPresence (String yearvalue){
+
+        Assert.assertTrue(driver.findElement(By.xpath("//li[@data-qa-selector-value='"+yearvalue+"'and@data-qa-selector='active-filter']")).isDisplayed());
+        System.out.println("Assertion on year filter successful");
+
+        return new SearchPage(driver);
+    }
+
+    public SearchPage assertPriceFilterPresence (String pricevalue){
+
+        Assert.assertTrue(driver.findElement(By.xpath("//li[@data-qa-selector-value='"+pricevalue+"'and@data-qa-selector='active-filter']")).isDisplayed());
+        System.out.println("Assertion on price filter successful");
 
         return new SearchPage(driver);
     }
